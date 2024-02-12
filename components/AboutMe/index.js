61d008ab2432c0a636e1385b55a0b5b4a770ad60
Elliott <Elliott.Samuel.Stevens@gmail.com>
2024-02-12 21:50:52 +0000

    Update site styling to make it more responsive

--- components/AboutMe/index.js
+++ components/AboutMe/index.js
@@ -7,10 +7,9 @@ import ProjectViewer from "../ProjectViewer/ProjectViewer";
 import classNames from "classnames";
 
 export default function AboutMe() {
-    const { ref, inView } = useInView();
-    const { ref: videoRef, inView: videoInView } = useInView();
     const { ref: aboutMeRef, inView: aboutMeInView } = useInView();
     const { ref: projectsRef, inView: projectsInView } = useInView();
+    const { ref: videoRef, inView: videoInView } = useInView();
 
     const {
         animateCard,
@@ -34,71 +33,66 @@ export default function AboutMe() {
         <div className={container}>
             <div
                 id="about-me"
-                className={aboutMeInView ? animateCard : ""}
+                className={classNames(aboutMeInView && animateCard)}
                 ref={aboutMeRef}
             >
                 <div className={section}>
                     <Banner />
                 </div>
-                <div className={content} ref={ref}>
+                <div className={content} ref={aboutMeRef}>
                     <div className={section}>
                         <div className={wrapper}>
-                            <div className={textSection}>
-                                <h2 className={title}>About me</h2>
-                                <div className={infoSection}>
-                                    <p className={text}>{aboutMe.text}</p>
-                                    <div className={imageWrapper}>
-                                        <img
-                                            className={image}
-                                            src={aboutMe.image}
-                                            alt="Elliott Stevens"
-                                        />
-                                    </div>
+                            <h2 className={title}>About me</h2>
+                            <div className={infoSection}>
+                                <p className={text}>{aboutMe.text}</p>
+                                <div className={imageWrapper}>
+                                    <img
+                                        className={image}
+                                        src={aboutMe.image}
+                                        alt="Elliott Stevens"
+                                    />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className={projectsInView ? animateCard : ""}>
-                <div className={section}>
-                    <div
-                        ref={projectsRef}
-                        className={classNames(wrapper, projectViewer)}
-                    >
-                        <h4 className={title}>Projects:</h4>
-                        <div className={infoSection}>
-                            <ProjectViewer />
+
+            <div className={classNames(section, projectsInView && animateCard)}>
+                <div
+                    ref={projectsRef}
+                    className={classNames(wrapper, projectViewer)}
+                >
+                    <h4 className={title}>Projects:</h4>
+                    <div className={infoSection}>
+                        <ProjectViewer />
+                    </div>
+                </div>
+            </div>
+
+            <div className={classNames(section, videoInView && animateCard)}>
+                <div
+                    className={classNames(projectVideoWrapper, wrapper)}
+                    ref={videoRef}
+                >
+                    <h3 className={title}>Bootcamp final project:</h3>
+                    <div className={infoSection}>
+                        <div className={projectVideo}>
+                            <iframe
+                                className={customIframe}
+                                width="560"
+                                height="315"
+                                src="https://www.youtube.com/embed/IUmr7r1vAXE"
+                                title="YouTube video player"
+                                allowFullScreen
+                            ></iframe>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className={videoInView ? animateCard : ""}>
-                <div className={section}>
-                    <div
-                        className={classNames(projectVideoWrapper, wrapper)}
-                        ref={videoRef}
-                    >
-                        <h3 className={title}>Bootcamp final project:</h3>
-                        <div className={infoSection}>
-                            <div className={projectVideo}>
-                                <iframe
-                                    className={customIframe}
-                                    width="560"
-                                    height="315"
-                                    src="https://www.youtube.com/embed/IUmr7r1vAXE"
-                                    title="YouTube video player"
-                                    allowFullScreen
-                                ></iframe>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className={videoInView ? animateCard : ""}>
-                <div className={section}>
-                    <Footer />
-                </div>
+
+            <div className={classNames(section, videoInView && animateCard)}>
+                <Footer />
             </div>
         </div>
     );
